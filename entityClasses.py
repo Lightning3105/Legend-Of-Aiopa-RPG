535513d6e9b266a297a41667bb4286dd75c854fd
James <james.waters@talktalk.net>
2015-07-19 17:22:31 +0100

    Added a hitbox to the player and a sword

--- entityClasses.py
+++ entityClasses.py
@@ -40,20 +40,20 @@ class SpriteSheet(object):
         all = []
         for h in range(self.rows):
             for w in range(self.columns):
-                print(w)
                 image = py.Surface([width, height], py.SRCALPHA, 32).convert_alpha()
                 image.blit(self.sprite_sheet, (0, 0), (w * width, h * height, width, height))
                 all.append(image)
         self.images = all
 
 
-class Player(py.sprite.Sprite): #TODO: Inherit from sprite
+class Player(py.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
         self.posx = screen.get_rect()[2] / 2
         self.posy = screen.get_rect()[3] / 2
         self.direction = "Down"
+        v.playerDirection = self.direction
         self.moving = False
         self.view = "DownC"
         self.sheetImage = "Resources\Images"
@@ -102,15 +102,33 @@ class Player(py.sprite.Sprite): #TODO: Inherit from sprite
 
     def move(self):
         py.event.pump()
+        moveRight = True
+        moveLeft = True
+        moveUp = True
+        moveDown = True
+        preX = v.playerPosX
+        preY = v.playerPosY
+        self.velX = 0
+        self.velY = 0
+        """for wall in v.wallHitList:
+            if self.rect.colliderect(wall.rect):
+                if self.rect.right > wall.rect.left:
+                    moveRight = False
+                if self.rect.left < wall.rect.right:
+                    moveLeft = False
+                if self.rect.top < wall.rect.bottom:
+                    moveUp = False
+                if self.rect.bottom > wall.rect.top:
+                    moveDown = False"""
         keys_pressed = py.key.get_pressed()
         if keys_pressed[py.K_a]:
-            v.playerPosX += 2
+            self.velX = 2
         if keys_pressed[py.K_d]:
-            v.playerPosX += -2
+            self.velX = -2
         if keys_pressed[py.K_s]:
-            v.playerPosY += -2
+            self.velY = -2
         if keys_pressed[py.K_w]:
-            v.playerPosY += 2
+            self.velY = 2
 
         if keys_pressed[py.K_s]:
             self.direction = "Down"
@@ -127,15 +145,60 @@ class Player(py.sprite.Sprite): #TODO: Inherit from sprite
         else:
             self.moving = False
 
+        v.playerPosX += self.velX
+        v.playerPosY += self.velY
+        v.playerDirection = self.direction
+
+
+        """for wall in v.hitList:
+            if self.rect.colliderect(wall.rect):
+                print("COLLIDE")
+                if self.rect.right > wall.rect.left:
+                    v.playerPosX = preX
+                    self.draw()"""
+
+
+class HitBox(py.sprite.Sprite):
+
+    def __init__(self, x, y, w, h, side):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.side = side
+        self.rect = py.Rect(x, y, w, h)
+        self.image = py.Surface((w, h))
+        self.image.fill((255, 0, 0))
+
+    def draw(self):
+        py.draw.rect(screen, (255, 0, 0), self.rect)
+
+    def update(self):
+        for thing in v.hitList:
+            if self.rect.colliderect(thing.rect):
+                if self.side == "Top":
+                    v.playerPosY += -2
+                if self.side == "Bottom":
+                    v.playerPosY += 2
+                if self.side == "Left":
+                    v.playerPosX += -2
+                if self.side == "Right":
+                    v.playerPosX += 2
+
+
 class Tile(py.sprite.Sprite):
 
-    def __init__(self, tilePosition, skin):
+    def __init__(self, tilePosition, skin, wall=False):
         super().__init__()
         self.tilePosX = tilePosition[0]
         self.tilePosY = tilePosition[1]
         self.posX = 0
         self.posY = 0
         self.image = skin
+        self.wall = wall
+        if wall:
+            v.hitList.add(self)
 
     def draw(self):
         self.set_rect()
@@ -146,3 +209,68 @@ class Tile(py.sprite.Sprite):
         self.rect = self.rend.get_rect()
         self.rect.centerx = screen.get_rect()[2] / 2 + (v.playerPosX + (30 * self.tilePosX))
         self.rect.centery = screen.get_rect()[3] / 2 + (v.playerPosY + (30 * self.tilePosY))
+
+
+class Sword:
+
+    def __init__(self):
+        self.image = None
+        self.attacking = False
+        self.attCyclePos = 0
+        self.attSpeed = 8
+        self.posX = screen.get_rect()[2] / 2
+        self.posY = screen.get_rect()[3] / 2
+
+    def get_rend(self):
+        self.rend = py.image.load(self.image)
+        self.rend = py.transform.scale(self.rend, (20, 20))
+
+    def update(self):
+        self.get_rend()
+        if self.attacking:
+            if v.playerDirection == "Up":
+                angleMod = -90
+            elif v.playerDirection == "Down":
+                angleMod = 90
+            elif v.playerDirection == "Left":
+                angleMod = 0
+            elif v.playerDirection == "Right":
+                angleMod = 180
+            if self.attCyclePos == 0:
+                self.rect = self.rend.get_rect()
+                self.rect.center = (self.posX, self.posY)
+                self.rend = rot_center(self.rend, angleMod + 180)
+                self.rect.center = arc((centre()[0], centre()[1]), 20, angleMod)
+                self.attCyclePos += self.attSpeed
+            else:
+                self.rend = rot_center(self.rend, angleMod + 180 - self.attCyclePos)
+                self.rect.center = arc((centre()[0], centre()[1]), 20, angleMod - self.attCyclePos)
+                self.attCyclePos += self.attSpeed
+                if self.attCyclePos > 180:
+                    self.attacking = False
+                    self.attCyclePos = 0
+        else:
+            self.rend = None
+            self.rect = None
+
+    def draw(self):
+        self.update()
+        if self.attacking:
+            screen.blit(self.rend, self.rect)
+
+def rot_center(image, angle):
+    """rotate an image while keeping its center and size"""
+    orig_rect = image.get_rect()
+    rot_image = py.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
+
+def arc(point, radius, degrees):
+    import math as m
+    out = (point[0] + (m.sin(m.radians(degrees)) * radius), point[1] + (m.cos(m.radians(degrees)) * radius))
+    return out
+
+def centre():
+    return screen.get_rect()[2] / 2, screen.get_rect()[3] / 2

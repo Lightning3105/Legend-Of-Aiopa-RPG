5705b777f38c15ccbb53e9e16954d6da5c286d47
James <james.waters@talktalk.net>
2015-10-02 18:40:46 +0100

    Added the beginnings of dragging items.

--- inventoryScreen.py
+++ inventoryScreen.py
@@ -2,20 +2,24 @@ import Variables as v
 import pygame as py
 import entityClasses
 
-class inventory():
+class inventory(): #TODO: Split into inventory and inventoryScreen
     
     def __init__(self):
         self.size = 3
         self.contents = []
+        self.grabbed = None
+        self.hovering = None
     
     def update(self):
+        self.hovering = None
         self.grey()
         self.background()
         self.player()
         self.equipedSlots("Weapon")
         for i in range(0, 24):
             self.inventorySlots(i)
-        self.grabbed = None
+        self.drag()
+        
         
     def grey(self):
         grey = py.Surface((v.screen.get_rect()[2], v.screen.get_rect()[3])).convert_alpha()
@@ -41,6 +45,7 @@ class inventory():
         v.screen.blit(image, pos)
     
     def equipedSlots(self, slot):
+        hovered = False
         item = v.equipped[slot]
         size = (50, 50)
         if slot == "Weapon":
@@ -51,7 +56,8 @@ class inventory():
         rect = py.Rect(pos, size)
         py.draw.rect(v.screen, (255, 255, 255), rect, 2)
         if rect.collidepoint(py.mouse.get_pos()):
-            pass
+            hovered = True
+            self.hovering = ("Equiped", slot)
         else:
             image.fill((255, 255, 255, 100), special_flags=py.BLEND_RGBA_MULT)
         v.screen.blit(image, pos)
@@ -59,9 +65,16 @@ class inventory():
         if not item == None:
             icon = py.transform.scale(item.icon, (size[0] - 2, size[1] - 2))
             v.screen.blit(icon, pos)
+            if hovered:
+                for event in v.events:
+                    if event.type == py.MOUSEBUTTONDOWN:
+                        self.grabbed = item
+        
+        
     
     def inventorySlots(self, slotNum):
         size = (50, 50)
+        hovered = False
         if slotNum >= len(self.contents):
             image = py.image.load("Resources/Images/Inventory Icons/Empty.png").convert_alpha()
         else:
@@ -79,7 +92,8 @@ class inventory():
         py.draw.rect(v.screen, (255, 255, 255), rect, 2)
         
         if rect.collidepoint(py.mouse.get_pos()):
-            pass
+            hovered = True
+            self.hovering = ("Inventory", slotNum)
         else:
             image.fill((255, 255, 255, 100), special_flags=py.BLEND_RGBA_MULT)
         v.screen.blit(image, pos)
@@ -88,6 +102,24 @@ class inventory():
             icon = py.transform.scale(self.contents[slotNum].icon, (size[0] - 4, size[1] - 4))
             icon.convert_alpha()
             v.screen.blit(icon, pos)
+            if hovered:
+                for event in v.events:
+                    if event.type == py.MOUSEBUTTONDOWN:
+                        self.grabbed = self.contents[slotNum]
         except:
             pass
+        
+        
+    def drag(self):
+        if not self.grabbed == None:
+            size = (50, 50)
+            image = self.grabbed.icon
+            image = py.transform.scale(image, size)
+            pos = py.mouse.get_pos()
+            v.screen.blit(image, pos)
+            rect = py.Rect(pos, size)
+            py.draw.rect(v.screen, (200, 200, 200), rect, 2)
+            if not py.mouse.get_pressed()[0]:
+                self.grabbed = None
+                                       
         

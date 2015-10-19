561b222d4a2abe50246943e8102ea435d372ca5e
James <james.waters@talktalk.net>
2015-10-19 20:33:28 +0100

    Added a variable reset function.

--- setupScripts.py
+++ setupScripts.py
@@ -23,3 +23,30 @@ def createGroups():
     v.abilityButtons = py.sprite.Group()
     v.xpGroup = py.sprite.Group()
     v.droppedItems = py.sprite.Group()
+
+def defaultVariables():
+    v.playerPosX = 0
+    v.playerPosY = 0
+    v.playerDirection = ""
+    v.playerHealth = 20
+    v.playerMana = 20
+    v.experience = {"XP": 0, "XPL": 1, "XPtoL": 10}
+    v.xpMod = 1.5
+    v.npcID = 1
+    v.scale = 2
+    v.skillPoints = 5
+    v.equipped = {"Weapon": None}
+    v.appearance = {"Body": "Resources/Images/Character Customisation/Body/white.png",
+                  "Face": "Resources/Images/Character Customisation/Face/whiteNormal.png",
+                  "Dress": "Resources/Images/Character Customisation/Dress/cloakBrown.png",
+                  "Hair": "Resources/Images/Character Customisation/Hair/Brown1.png"
+                  }
+    v.Attributes = {"Speed": 0,
+                  "Max Health": 0,
+                  "Defence": 0,
+                  "Strength": 0,
+                  "Max Mana": 0,
+                  "Magical Strength": 0,
+                  "Luck": 0,
+                  "Charisma": 0
+                  }

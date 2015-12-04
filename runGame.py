5dd5afe2d040b6de9f472a7d815790d6fa56f425
Lightning3105 <james.waters@talktalk.net>
2015-12-04 17:29:28 +0000

    Added line to say when the game is running.

--- runGame.py
+++ runGame.py
@@ -2,6 +2,7 @@ import gameScreens
 import setupScripts
 import cProfile
 import pstats
+print("THE GAME IS NOW RUNNING")
 if __name__ == "__main__":
     cProfile.run("gameScreens.mainMenu()", "Out.txt")
     p = pstats.Stats("Out.txt")

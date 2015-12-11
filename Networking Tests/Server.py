6b09829bb2b9c3e7bf5d3143ecc87ce3278421ba
James <james.waters@talktalk.net>
2015-12-11 17:10:20 +0000

    Tried some stuff that didn't work.

--- Networking Tests/Server.py
+++ Networking Tests/Server.py
@@ -9,13 +9,14 @@ serversocket = socket.socket(
             socket.AF_INET, socket.SOCK_STREAM) 
 
 # get local machine name
-host = socket.gethostname()                           
+host = socket.gethostname()     
+#host = "80.42.171.141"          
+#host = "192.168.1.1"         
 
-port = 9999                                           
-
+port = 31055                                           
 # bind to the port
 serversocket.bind((host, port))                                  
-
+print("Started server on ", str(host) + ":" + str(port))
 # queue up to 5 requests
 serversocket.listen(2)
 

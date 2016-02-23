71517af1332ccd49c7dbd7426e56f702153f12c2
James <james.waters@talktalk.net>
2016-02-23 22:21:27 +0000

    Function Calltime Stats
    Began work on a system of compiling, averaging and uploading the
    function calltime statistics to the web server.

--- runGame.py
+++ runGame.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+def tree(d, indent=0):
+    for key, value in d.items():
+        print ('\t' * indent + str(key))
+        if isinstance(value, dict):
+            tree(value, indent+1)
+        else:
+            print ('\t' * (indent+1) + str(value))
+
+
 if __name__ == "__main__":
     import Updater
     from getpass import getuser
@@ -14,11 +23,22 @@ if __name__ == "__main__":
     import setupScripts
     import cProfile
     import pstats
+    import pickle
+    
     print("START GAME")
     cProfile.run("gameScreens.mainMenu()", "Out.txt")
     with open("Calltime Dump.txt", "w") as fc:
         p = pstats.Stats("Out.txt", stream=fc)
-        p.strip_dirs().sort_stats("time").print_stats()
+        p.strip_dirs()
+        d = p.__dict__["stats"]
+        #print(d)
+        
+        for k, v in d.items():
+            print(k, ":::", v)
+        p.sort_stats("time").print_stats()
+        
+    with open("Calltime Dump.txt", "rb") as fc:
+        pass
     #gameScreens.story()
     
     #gameScreens.game()

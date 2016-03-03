d3ccf68a615ba33a176f3c7dded4432d1dca91d4
James <james.waters@talktalk.net>
2016-03-03 21:20:39 +0000

    Fixed updater
    Fixed buttons not pressing

--- Updater.py
+++ Updater.py
@@ -10,90 +10,87 @@ except:
     v = var()
 
 import pygame as py
-try:
-    from MenuItems import Button, textLabel
-except:
-    class textLabel(py.sprite.Sprite):
-        def __init__(self, text, pos, colour, font, size, variable = False, centred = False):
-            super().__init__()
-            self.text = text
-            self.pos = pos
-            self.colour = colour
-            self.font = font
-            self.size = size
-            self.variable = variable
-            self.centred = centred
-            
-        def update(self):
-            pos = self.pos
-            font = py.font.Font(self.font, self.size)
-            if not self.variable:
-                label = font.render(self.text, 1, self.colour)
-            if self.variable:
-                label = font.render(str(getattr(v, self.text)), 1, self.colour)
-            if self.centred:
-                pos = list(self.pos)
-                pos[0] -= font.size(self.text)[0] / 2
-                pos[1] -= font.size(self.text)[1] / 2
-                pos = tuple(pos)
-            v.screen.blit(label, pos)
-    class Button(py.sprite.Sprite):
-        def __init__(self, text, pos, size, hovercolour, normalcolour, font, ID, centred = False, bsize=(0,0)):
-            super().__init__()
-            self.ID = ID
-            self.hovered = False
-            self.text = text
-            self.pos = pos
-            self.hcolour = hovercolour
-            self.ncolour = normalcolour
-            self.font = font
-            self.font = py.font.Font(font, int(size))
-            self.centred = centred
-            self.size = bsize
-            self.set_rect()
+class textLabel(py.sprite.Sprite):
+    def __init__(self, text, pos, colour, font, size, variable = False, centred = False):
+        super().__init__()
+        self.text = text
+        self.pos = pos
+        self.colour = colour
+        self.font = font
+        self.size = size
+        self.variable = variable
+        self.centred = centred
         
-        def update(self):
-            self.set_rend()
-            py.draw.rect(v.screen, self.get_color(), self.rect)
-            v.screen.blit(self.rend, self.rect)
-            if self.rect.collidepoint(py.mouse.get_pos()):
-                self.hovered = True
-            else:
-                self.hovered = False
+    def update(self):
+        pos = self.pos
+        font = py.font.Font(self.font, self.size)
+        if not self.variable:
+            label = font.render(self.text, 1, self.colour)
+        if self.variable:
+            label = font.render(str(getattr(v, self.text)), 1, self.colour)
+        if self.centred:
+            pos = list(self.pos)
+            pos[0] -= font.size(self.text)[0] / 2
+            pos[1] -= font.size(self.text)[1] / 2
+            pos = tuple(pos)
+        v.screen.blit(label, pos)
+class Button(py.sprite.Sprite):
+    def __init__(self, text, pos, size, hovercolour, normalcolour, font, ID, centred = False, bsize=(0,0)):
+        super().__init__()
+        self.ID = ID
+        self.hovered = False
+        self.text = text
+        self.pos = pos
+        self.hcolour = hovercolour
+        self.ncolour = normalcolour
+        self.font = font
+        self.font = py.font.Font(font, int(size))
+        self.centred = centred
+        self.size = bsize
+        self.set_rect()
     
-        def set_rend(self):
-            self.rend = self.font.render(self.text, True, (0,0,0))
-    
-        def get_color(self):
-            if self.hovered:
-                return self.hcolour
-            else:
-                return self.ncolour
-    
-        def set_rect(self):
-            self.set_rend()
-            self.rect = self.rend.get_rect()
-            if not self.centred:
-                self.rect.topleft = self.pos
-            if self.centred:
-                self.rect.center = self.pos
-            
-            if not self.size[0] == 0:
-                self.rect.width = self.size[0]
-            if not self.size[1] == 0:
-                self.rect.height = self.size[1]
-    
-        def pressed(self):
-            mouse = py.mouse.get_pos()
-            if mouse[0] > self.rect.topleft[0]:
-                if mouse[1] > self.rect.topleft[1]:
-                    if mouse[0] < self.rect.bottomright[0]:
-                        if mouse[1] < self.rect.bottomright[1]:
-                            return True
-                        else: return False
+    def update(self):
+        self.set_rend()
+        py.draw.rect(v.screen, self.get_color(), self.rect)
+        v.screen.blit(self.rend, self.rect)
+        if self.rect.collidepoint(py.mouse.get_pos()):
+            self.hovered = True
+        else:
+            self.hovered = False
+
+    def set_rend(self):
+        self.rend = self.font.render(self.text, True, (0,0,0))
+
+    def get_color(self):
+        if self.hovered:
+            return self.hcolour
+        else:
+            return self.ncolour
+
+    def set_rect(self):
+        self.set_rend()
+        self.rect = self.rend.get_rect()
+        if not self.centred:
+            self.rect.topleft = self.pos
+        if self.centred:
+            self.rect.center = self.pos
+        
+        if not self.size[0] == 0:
+            self.rect.width = self.size[0]
+        if not self.size[1] == 0:
+            self.rect.height = self.size[1]
+
+    def pressed(self):
+        mouse = py.mouse.get_pos()
+        if mouse[0] > self.rect.topleft[0]:
+            if mouse[1] > self.rect.topleft[1]:
+                if mouse[0] < self.rect.bottomright[0]:
+                    if mouse[1] < self.rect.bottomright[1]:
+                        return True
                     else: return False
                 else: return False
             else: return False
+        else: return False
 import os, shutil
 
 try:

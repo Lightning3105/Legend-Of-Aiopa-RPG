3733b6294716bcde3a7270e68ce14c8528c6de8b
James <james.waters@talktalk.net>
2016-03-14 19:22:42 +0000

    Removed Automatic Updating From Run Game
    It only works with source

--- runGame.py
+++ runGame.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python3
 if __name__ == "__main__":
-    import Updater
-    from getpass import getuser
-    if not getuser() == "James Waters":
-        print(getuser() == "James Waters")
-        try:
-            Updater.updateCheck()
-        except:
-            pass
-    
     import gameScreens
     import setupScripts
     import cProfile
